dee6b1df9b020b01201b2f89a75c21c2850d876e
shekynab <113492533+shekynab@users.noreply.github.com>
2022-12-13 01:30:01 +0800

    Rename script.text.rtf to script.js

---
+++ Wordpad/script.js
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1252\deff0\nouicompat\deflang1033{\fonttbl{\f0\fnil\fcharset0 Calibri;}}
+{\*\generator Riched20 10.0.19041}\viewkind4\uc1 
+\pard\sa200\sl276\slmult1\f0\fs22\lang9 $(document).ready(function()\{\par
+    $(window).scroll(function()\{\par
+        if(this.scrollY > 20)\{\par
+            $('.navbar').addClass("sticky");\par
+        \}else\{\par
+            $('.navbar').removeClass("sticky");\par
+        \}\par
+    \});\par
+    \par
+    $('.menu-btn').click(function()\{\par
+        $('.navbar .menu').toggleClass("active");\par
+    \});\par
+\});\par
+\par
+}
+�
